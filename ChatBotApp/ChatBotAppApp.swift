88fdaef7af7f85405f71561e450f938c8c4f859e
Yuriy Pashkov <yuriy.pashkov@gmail.com>
2023-12-21 12:15:30 +0700

    first commit

---
+++ ChatBotApp/ChatBotAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBotAppApp.swift
+//  ChatBotApp
+//
+//  Created by Yuriy Pashkov on 21.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatBotAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
